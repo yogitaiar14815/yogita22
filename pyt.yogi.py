12ca434269d76f29d984c9ff3ce61c592d764e36
yogitaiar14815 <yogitabazala6@gmail.com>
2025-04-20 18:39:44 +0530

    Add files via upload

---
+++ pyt.yogi.py
@@ -0,0 +1,5 @@
+Python 3.12.9 (tags/v3.12.9:fdb8142, Feb  4 2025, 15:27:58) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = "hello world"
+>>> print(a[1:5])
+ello
